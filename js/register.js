94554defa8c0664308c436c4e0b7c93dbae8b310
kreme <1401005429@qq.com>
2018-09-15 11:55:22 +0800

    提交注册事件(1)

--- js/register.js
+++ js/register.js
@@ -1,3 +1,14 @@
 /**
  * Created by Administrator on 2018/9/15.
  */
+$(function() {
+    $('#register-btn').on('click', function() {
+        //alert(111);
+        //获取表单元素中用户输入的值
+        var username = $('[name="username"]').val();
+        var mobile = $('[name="mobile"]').val();
+        var password = $('[name="password"]').val();
+        var againPass = $('[name="againPass"]').val();
+        var vCode = $('[name="vCode"]').val();
+    })
+})
