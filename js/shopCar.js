a1c651ac7194e8c603b073ea4ff52a62ea3eb801
kreme <1401005429@qq.com>
2018-09-18 20:14:58 +0800

    商品详情和购物车功能优化

--- js/shopCar.js
+++ js/shopCar.js
@@ -12,30 +12,21 @@ $(function(){
         url:'/cart/queryCart',
         type:'get',
         success:function(result){
-
-            if(result.error && result.error == 400){
-
-                localStorage.setItem('returnUrl',location.href);
-
-                location.href = "login.html";
-
-                return;
-
-            }
-
-            carts = result;
-
             console.log(result);
-
-            $('#cartBox').html(template('cartTpl',{data:result}))
-
+            if(result.error && result.error == 400){
+                localStorage.setItem('returnUrl',location.href);
+                location.href = "login.html";
+                return;
+            }
+            carts = result;
+            //console.log(result);
+            $('#cartBox').html(template('cartTpl',{data:result}));
         }
     })
 
+    //删除事件
     $('body').on('tap','.deleteBtn',function(){
-
         var id = $(this).attr('data-id');
-
         $.ajax({
             url:'/cart/deleteCart',
             type:'get',
@@ -43,56 +34,34 @@ $(function(){
                 id:[id]
             },
             success:function(result){
-
                 if(result.error && result.error == 400){
-
                     localStorage.setItem('returnUrl',location.href);
-
                     location.href = "login.html";
-
                     return;
-
                 }
-
                 if(result.success) location.reload();
-
-
             }
         })
-
     });
-
+    //编辑事件
     $('body').on('tap','.editBtn',function(){
-
         var id = $(this).attr('data-id');
-
         var item = findGoodsById(carts,id);
-
         productNum = item.productNum;
         num = item.num;
         size = item.size;
         var productSize = item.productSize.split('-');
         var start = parseInt(productSize[0]);
         var end = parseInt(productSize[1]);
-
         item.customSize = [];
-
         for(var i=start;i<=end;i++){
-
             item.customSize.push(i);
-
         }
-
-        console.log(item)
-
+        //console.log(item)
         var html = template('detailTpl',{data:item}).replace(/\n/g,'');
-
         var btnArray = ['确定', '取消'];
-
         mui.confirm(html, '编辑商品', btnArray, function(e) {
-
             if (e.index == 0) {
-
                 $.ajax({
                     url:'/cart/updateCart',
                     type:'post',
@@ -102,77 +71,56 @@ $(function(){
                         num:num
                     },
                     success:function(result){
-
                         if(result.success){
-
-                            alert('修改成功');
-
-                            location.reload();
-
+                            mui.toast('修改成功');
+                            setTimeout(function() {
+                                location.reload();
+                            },1500)
                         }
-
                     }
                 })
-
             } else {
                 alert('MUI没有得到你的认可，继续加油')
             }
         })
-
     });
-
-
+    //型号高亮
     $('body').on('tap','.detail-size span',function(){
-
         $(this).addClass('active').siblings().removeClass('active');
-
         size = $(this).html();
-
     });
-
-
+    //数量减
     $('body').on('tap','.detail-num .reduce',function(){
-
         num = $('.detail-num .num').val();
-
         num--;
-
         if(num<0){
             num=0;
         }
-
         $('.detail-num input').val(num);
-
-    })
-
+    });
+    //数量加
     $('body').on('tap','.detail-num .plus',function(){
-
         num = $('.detail-num .num').val();
-
         num++;
-
         if(num>productNum){
             num=productNum;
         }
-
         $('.detail-num input').val(num);
-
-
     })
 
-
+    $('#left-refresh').on('click', function() {
+        location.href = 'search-result.html';
+    })
+    $('#right-refresh').on('click', function() {
+        window.location.reload();
+    })
 })
 
 function findGoodsById(result,id){
-    console.log(result)
+    //console.log(result)
     for(var i=0;i<result.length;i++){
-
         if(result[i].id == id){
-
             return result[i];
-
         }
-
     }
-
 }
